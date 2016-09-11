ba2483c601c46b0810a1764fad1bfbec8b9677f4
Dan McHenry <danpmch@gmail.com>
2016-09-11 14:10:15 -0400

    Added rudimentary command line interface

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import System.Environment
+import Codec.Picture.Png
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+   args <- getArgs
+   let files = parse args
+       result = fmap (uncurry steg) files
+   case result of
+        (Right result) -> putStrLn result
+        (Left error) -> putStrLn error
+
+parse :: [String] -> Either String (String, String)
+parse (fileToHide : fileToHideIn : _) = Right (fileToHide, fileToHideIn)
+parse _ = Left "Usage: ImageSteg  <file to hide>  <file to hide in>"
+
+steg :: String -> String -> String
+steg _ _ = "TODO"
+
