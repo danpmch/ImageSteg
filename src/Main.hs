361c156e72f4d78a24e02b018d906e86cd3b4073
Dan McHenry <danpmch@gmail.com>
2016-09-11 15:35:52 -0400

    overly complicated way to read images

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,40 @@ module Main where
 
 import System.Environment
 import Codec.Picture.Png
+import Codec.Picture.Types
+import qualified Data.ByteString as B
+
+data StegArg a = StegArg a a
+
+instance Functor StegArg where
+   fmap f (StegArg a1 a2) = StegArg (f a1) (f a2)
+
+instance Foldable StegArg where
+   foldr f acc (StegArg a1 a2) = a1 `f` (a2 `f` acc)
+   foldMap f (StegArg a1 a2) = mempty `mappend` (f a1) `mappend` (f a2)
+
+instance Traversable StegArg where
+   traverse f (StegArg a1 a2) = (pure StegArg) <*> (f a1) <*> (f a2)
+   sequenceA (StegArg fa1 fa2) = (pure StegArg) <*> fa1 <*> fa2
 
 main :: IO ()
 main = do
    args <- getArgs
-   let files = parse args
-       result = fmap (uncurry steg) files
-   case result of
-        (Right result) -> putStrLn result
+   let filenames = parse args
+   files <- sequence . fmap getFiles $ filenames
+   let image = files >>= steg
+   case image of
+        (Right image) ->
+           writePng "test.png" image
         (Left error) -> putStrLn error
 
-parse :: [String] -> Either String (String, String)
-parse (fileToHide : fileToHideIn : _) = Right (fileToHide, fileToHideIn)
+getFiles :: StegArg String -> IO (StegArg B.ByteString)
+getFiles = sequence . fmap B.readFile
+
+parse :: [String] -> Either String (StegArg String)
+parse (fileToHide : fileToHideIn : _) = Right (StegArg fileToHide fileToHideIn)
 parse _ = Left "Usage: ImageSteg  <file to hide>  <file to hide in>"
 
-steg :: String -> String -> String
-steg _ _ = "TODO"
+steg :: StegArg B.ByteString -> Either String Codec.Picture.Types.DynamicImage
+steg (StegArg fileToHide fileToHideIn) = decodePng fileToHide
 
